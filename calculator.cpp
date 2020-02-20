895c69852872c07641863ca0a9f23522e976466c
arqdsis <arqdsis@MOOLIi0106.animaedu.intranet>
2020-02-20 20:45:59 -0300

    somente soma

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+float a,b,c,d;
+printf ("\n digite um numero");
+scanf("%f",&a);
+printf("\n digite 1para soma 2 para subtracao 3 para multiplicao 4 para divisao ");
+scanf("%f",&b);
+printf("\n digite outro numero");
+scanf ("%f",&c);
+if(b==1)
+        {       
+           d= a+c;
+           printf ("resultado da soma = %f",d);
+        }
+system("PAUSE");
+}
+
